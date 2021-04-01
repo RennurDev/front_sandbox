23c585cf9d9b2958c0ac8a2ef43b1e86dedaff92
hatake511522 <623dgk511522@gmail.com>
2021-04-01 16:46:28 +0900

    クリックされたボタンによって表示される内容を変更する

--- src/components/menu/App.js
+++ src/components/menu/App.js
@@ -8,21 +8,40 @@ import LogsContent from './content/Logs';
 import Grid from '@material-ui/core/Grid';
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      selectedContent: '',
+    }
+  }
+  
   render() {
+    let {selectedContent} = this.state;
+    const selectProfile = () => this.setState({ selectedContent: 'Profile' })
+    const selectLogs = () => this.setState({selectedContent: 'Logs'})
     return(
       <div>
         <Grid container spacing={2}>
           <Grid item xs={6}>
-            <Profile></Profile>
+            <div onClick={selectProfile}>
+              <Profile></Profile>
+            </div>
           </Grid>
           <Grid item xs={6}>
-            <Logs></Logs>
+            <div onClick={selectLogs}>
+              <Logs></Logs>
+            </div>
           </Grid>
         </Grid>
 
-
-        <ProfileContent></ProfileContent>  
-        <LogsContent></LogsContent>
+        {selectedContent === 'Profile' ? 
+         <ProfileContent></ProfileContent>
+         : 
+         selectedContent === 'Logs' 
+         ? <LogsContent></LogsContent> 
+         : 
+         null}
       </div>
     )
   }
