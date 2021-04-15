f8e016cc30c131b06d73aad55e7e47643838a2a6
k1105 <kanapon1105@yahoo.co.jp>
2021-04-15 19:08:23 +0900

    react routerの追加

--- src/App.js
+++ src/App.js
@@ -4,18 +4,38 @@ import MapBox from './components/map/MapBox';
 import Menu from './components/menu/App';
 import './App.css'
 import UserForm from './components/user/App'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 class App extends Component {
 
   render(){
     return (
       <div>
-        <UserForm></UserForm>
-        
-        {/* <Header></Header>
-        <MapBox></MapBox>
-        <Menu></Menu> */}
+        <Router>
+          <ul>
+            <li>
+              <Link to='/home'>Home</Link>
+            </li>
+            <li>
+              <Link to='/sign_up'>Sign up</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/sign_up">
+              <UserForm />
+            </Route>
+            <Route path="/home">
+              <Header />
+              <MapBox />
+              <Menu />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     )
   }
