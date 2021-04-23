6716fe4396ec3d040ad2d373716fd9ea77c74a5f
k1105 <kanapon1105@yahoo.co.jp>
2021-04-23 18:51:46 +0900

    geolocation apiを使用したものへの移行

--- src/components/map/MapBox.js
+++ src/components/map/MapBox.js
@@ -21,13 +21,20 @@ export default class MapBox extends Component {
     this.state = {
       area: '',
       isStarted: false,
+      current_pos: {
+        lng: 0,
+        lat: 0,
+      }
     }
     this.history = []
     this.previous_location = undefined
     this.min_duration = 2
+    //watchPositionの実行idを管理
+    this.watch_id = -1
 
     this.onGeolocate = this.onGeolocate.bind(this) // これないと動かない
     this.onClick = this.onClick.bind(this);
+    this.setMap = this.setMap.bind(this);
   }
 
   _hasProperty(obj, prop) {
@@ -35,73 +42,73 @@ export default class MapBox extends Component {
   }
 
   _add(geolocate) {
-    if (this._hasProperty(geolocate, 'timestamp')) {
-      //console.log(geolocate)
-      this.history.push([geolocate.coords.longitude, geolocate.coords.latitude])
-    }
+    this.history.push([geolocate.coords.longitude, geolocate.coords.latitude])
   }
-  
-  addGeolocate(geolocate) {
-    const elapseTime = this.state.isStarted !== false ? parseInt((geolocate.timestamp - this.previous_location.timestamp)) : 0
 
-    if (this.state.isStarted) {
-      //console.log(geolocate)
-      this._add(geolocate) // 測り始め
-      this.previous_location = geolocate;
-    } else if (elapseTime > this.min_duration) {
-      this._add(geolocate) // 経過時間が設定した制限時間をこえたらヒストリ追加
-      this.previous_location = geolocate
+  setMap(position){ // 現在地取得
+    this.setState({
+      current_pos: {
+        lng: position.coords.longitude,
+        lat: position.coords.latitude,
+      }
+    })
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      center: [this.state.current_pos.lng, this.state.current_pos.lat],
+      style: 'mapbox://styles/mapbox/streets-v9', // mapのスタイル指定
+      zoom: 16
+    })
+
+    this.map.addControl(geolocate);
+  }
+
+  addGeolocate(position) {
+    console.log("position.timestamp: "+ position.timestamp)
+    console.log("previous timestamp: "+this.previous_location.timestamp)
+
+    const elapseTime = this.state.isStarted === true ? parseInt((position.timestamp - this.previous_location.timestamp)) : 0
+    console.log(elapseTime)
+    if (elapseTime > this.min_duration) {
+      this._add(position) // 経過時間が設定した制限時間をこえたらヒストリ追加
+      this.previous_location = position
     } else {
       return;
     }
   }
 
-  getUserTrack() {
-    const t = []
-    for (const item of this.history) {
-      t.push([item.coords.longitude, item.coords.latitude])
-    }
-    return t
-  }
-
   onClick() {
     let isStarted = this.state.isStarted
-    console.log(isStarted);
 
     if(isStarted) { //Record時の処理
       console.log(this.history);
+      console.log("id: " + this.watch_id);
+      navigator.geolocation.clearWatch(this.watch_id);
       //responseが帰ってきたらhistoryを初期化
       if (true) {
         this.history = []
       }
     } else { //Start時の処理
       //ここで描画レイヤーの初期化
-      console.log(this.history);
+      initializeGeoLine(this.map);
+      //console.log(this.history);
+      this.watch_id = navigator.geolocation.watchPosition(this.onGeolocate);
+      console.log("id: "+ this.watch_id)
     }
 
     this.setState({isStarted: !isStarted})
   }
   
   onGeolocate(position) {
-    console.log(this.history.length);
     if(this.history.length === 0) {
-      initializeGeoLine(this.map)
       this.previous_location = position;
+      this.history.push([position.coords.longitude, position.coords.latitude])
     }
     this.addGeolocate(position)
     drawGeoLine(this.history, this.map)
   }
 
   componentDidMount() {
-    this.map = new mapboxgl.Map({
-      container: this.mapContainer,
-      center: [-96, 37.8],
-      style: 'mapbox://styles/mapbox/streets-v9', // mapのスタイル指定
-      zoom: 8 // おそらく
-    })
- 
-    this.map.addControl(geolocate);
-    geolocate.on('geolocate', this.onGeolocate);
+    navigator.geolocation.getCurrentPosition(this.setMap)
   }
 
   componentWillUnmount() {
