a014c9be3865a4b5dce9a90b960622bc5939996b
nakamuuuura <big.tn.9672tf@gmail.com>
2021-06-10 21:05:04 +0900

    マップのbottomとtopににペタンプ挿入

--- src/components/header/UserLocation.js
+++ src/components/header/UserLocation.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 export default class UserInfo extends Component {
   render() {
     return (
-      <div>
-        <Grid container spacing={3}>
+     <div>
+       <Grid container alignContent="center" alignItems="center" justify="center">
           <Grid item xs={12}>
-            <p>御茶ノ水</p>
+            <div className="BackgroundAreaName">
+              <Grid container alignItems="center" justify="center">
+                  <Grid xs={7}>
+                    <img src={process.env.PUBLIC_URL+"/PetampAreaNameBackground-02.svg"}  alt="アイコン" />
+                  </Grid>
+              </Grid>
+            </div>
           </Grid>
+       </Grid>
+      <Grid container alignContent="center" alignItems="center" justify="center">
+        <Grid item xs={12}>
+          <div className="areaName">
+            <Typography className="textWhite" align="center" gutterBottom>
+              BUNKYO
+            </Typography>
+          </div>
         </Grid>
-        
-      </div>
+      </Grid>
+    </div>
     )
   }
 }
