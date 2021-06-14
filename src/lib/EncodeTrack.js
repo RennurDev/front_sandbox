8ee2a55c3769c3966c4754b267f0c19dcefab359
k1105 <kanapon1105@yahoo.co.jp>
2021-06-14 17:37:50 +0900

    try, catchの処理の消去

--- src/lib/EncodeTrack.js
+++ src/lib/EncodeTrack.js
@@ -1,16 +1,12 @@
 export default function EncodeTrack(data) {
   if (data !== []) {
-    try {
-      let encoded_track = ""
-      for(let i=0; i<data.length-1; i++) {
-        encoded_track += data[i]+":"
-      }
-      encoded_track += data[data.length-1]
-      // lng,lat:lng,lat...の形式に変換
-      return encoded_track
-    } catch(e) {
-      console.log(e)
+    let encoded_track = ""
+    for(let i=0; i<data.length-1; i++) {
+      encoded_track += data[i]+":"
     }
+    encoded_track += data[data.length-1]
+    // lng,lat:lng,lat...の形式に変換
+    return encoded_track
   } else {
     console.log("error happend while encoding track")
   }
