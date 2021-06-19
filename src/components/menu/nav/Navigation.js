3bd4ecf37da916cfef7b5ff0e3c4f18836177622
hatake511522 <623dgk511522@gmail.com>
2021-06-19 18:44:57 +0900

    fix lint

--- src/components/menu/nav/Navigation.js
+++ src/components/menu/nav/Navigation.js
@@ -1,33 +1,45 @@
-import React, { Component } from 'react';
-import {
-    BottomNavigation,
-    BottomNavigationAction,
-} from '@material-ui/core';
+import React, { Component } from "react";
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
 //Icons
-import PersonIcon from '@material-ui/icons/Person';
-import TimelineIcon from '@material-ui/icons/Timeline';
-import SettingsIcon from '@material-ui/icons/Settings';
+import PersonIcon from "@material-ui/icons/Person";
+import TimelineIcon from "@material-ui/icons/Timeline";
+import SettingsIcon from "@material-ui/icons/Settings";
 import { withStyles } from "@material-ui/core/styles";
 
-const styles = theme => ({
-    root: {
-      backgroundColor: "#00a563",
-    },
-  });
+const styles = (theme) => ({
+  root: {
+    backgroundColor: "#00a563",
+  },
+});
 
 class Navigation extends Component {
-    render() {
-        const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
 
-        return(
-            <BottomNavigation className={ classes.root } value={ this.props.value } onChange={ this.props.handleNavChange }>
-            <BottomNavigationAction label="Profile" value="Profile" icon={<PersonIcon />} />
-            <BottomNavigationAction label="Tracks" value="Tracks" icon={<TimelineIcon />} />
-            <BottomNavigationAction label="Setting" value="Setting" icon={<SettingsIcon />} />
-            </BottomNavigation>
-        )
-        
-    }
+    return (
+      <BottomNavigation
+        className={classes.root}
+        value={this.props.value}
+        onChange={this.props.handleNavChange}
+      >
+        <BottomNavigationAction
+          label="Profile"
+          value="Profile"
+          icon={<PersonIcon />}
+        />
+        <BottomNavigationAction
+          label="Tracks"
+          value="Tracks"
+          icon={<TimelineIcon />}
+        />
+        <BottomNavigationAction
+          label="Setting"
+          value="Setting"
+          icon={<SettingsIcon />}
+        />
+      </BottomNavigation>
+    );
+  }
 }
 
 export default withStyles(styles, { withTheme: true })(Navigation);
