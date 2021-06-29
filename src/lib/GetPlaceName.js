ba621954c6a230a5882f9decd923932b85bcb628
k1105 <kanapon1105@yahoo.co.jp>
2021-06-29 10:32:54 +0900

    東京都以外での地域名取得に対応する

---
+++ src/lib/GetPlaceName.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export default async function GetPlaceName(lng, lat) {
+  const location = lng+","+lat;
+  const access_token = process.env.REACT_APP_MAPBOX_API_KEY;
+  //NOTE: responseは狭い地域順に返ってくるので, 地域名を取得する場合はresponse.data.features[0].text
+  const options = "types=locality,place,region";
+  const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+location+".json?"+options+"&access_token="+access_token;
+  try {
+    const response = await axios.get(url)
+    return response
+  } catch(err) {
+    return err
+  }
+}
