4e034e711ca9fe026d41ecc64655101df727e851
k1105 <kanapon1105@yahoo.co.jp>
2021-07-15 16:29:13 +0900

    デバッグ用alertの追加

--- src/lib/CalcDistance.js
+++ src/lib/CalcDistance.js
@@ -5,9 +5,15 @@ export default function CalcDistance(pos_prev, pos_current) {
     const lng_c = pos_current.coords.longitude
     const lat_c = pos_current.coords.latitude
 
+    alert([lng_p, lat_p, lng_c, lat_c])
+
+    const distance = 6371 * Math.acos(
+      Math.cos(lat_c/180*pi) * Math.cos((lng_p - lng_c)/180*pi) * Math.cos(lat_p/180*pi) +
+      Math.sin(lat_c/180*pi) * Math.sin(lat_p/180*pi)
+    ) * 1000;
+
+    alert(distance);
+
     /*地球を半径6371kmの球体と仮定. 距離[m]=6371[km]*(２地点のなす角[rad])*1000 */
-    return 6371 * Math.acos(
-        Math.cos(lat_c/180*pi) * Math.cos((lng_p - lng_c)/180*pi) * Math.cos(lat_p/180*pi) +
-        Math.sin(lat_c/180*pi) * Math.sin(lat_p/180*pi)
-      ) * 1000;
+    return distance;
 }
