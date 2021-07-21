19f6547c9deab7c79a8abdffd602b57ba3a4bb5f
k1105 <kanapon1105@yahoo.co.jp>
2021-07-21 10:40:39 +0900

    Appjsの関数コンポーネント化

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import { Header } from "./header";
 import { Menu } from "./menu";
 import MapBox from "./components/map/MapBox";
 import "./App.css";
 import UserForm from "./components/user/App";
-import RequestAxios from "./lib/RequestAxios";
-import { withStyles } from "@material-ui/core/styles";
 
 const styles = (theme) => ({
   root: {
@@ -13,116 +11,40 @@ const styles = (theme) => ({
   },
 });
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      current_user: {
-        id: "",
-        name: "",
-      },
-      current_location: "",
-      form: {
-        name: "",
-      },
-      track_num: "0", //全Track数
-      tracks: [],
-      map: "",
-    };
-    this.getCurrentUser = this.getCurrentUser.bind(this);
-    this.handleUserLogin = this.handleUserLogin.bind(this); //TODO: 認証機能が完成すると不要になるかもしれない
-    this.handleState = this.handleState.bind(this);
-  }
+export const App = () => {
+  const [currentUser, setCurrentUser] = useState([{
+    id: "",
+    name: "",
+  }]);
+  const [currentLocation, setCurrentLocation] = useState("");
+  const [form, setForm] = useState([{
+    name: "",
+  }]);
+  const [trackNum, setTrackNum] = useState(0);
+  const [tracks, setTracks] = useState([]);
+  const [map, setMap] = useState("");
 
-  getCurrentUser() {
-    //TODO: device導入後, state.current_user.idを現在ログイン中のidで更新する処理を追記
-    let id = this.state.current_user.id;
-    const url = "/users/" + id;
-    let response = RequestAxios(url, "get");
-    response.then((r) => {
-      this.setState({ current_user: r });
-      this.setState({
-        name: this.state.current_user.name,
-      });
-    });
-  }
-
-  handleState(name, data) {
-    this.setState({ [name]: data });
-  }
-
-  handleUserLogin(id) {
-    //TODO: 認証機能が完成すると不要になるかもしれない
-    this.setState({
-      current_user: {
-        id: id,
-      },
-    });
-  }
-
-  // TODO: User API 実装後に修正
-  // //formの入力内容の変更を検知
-  // handleProfileChange(e, key) {
-  //   let target = e.target;
-  //   let value = target.value;
-  //   let form = this.state.form;
-  //   form[key] = value;
-
-  //   this.setState({
-  //     form: form,
-  //   });
-  // }
-
-  // //更新処理
-  // handleProfileUpdate() {
-  //   let body = {
-  //     user: {
-  //       id: this.state.current_user.id,
-  //       name: this.state.form.name,
-  //     },
-  //   };
-  //   let id = this.state.current_user.id;
-  //   const url = "/users/" + id;
-  //   let response = RequestAxios(url, "put", body);
-  //   response.then((r) => {
-  //     this.setState({
-  //       current_user: {
-  //         name: this.state.form.name,
-  //       },
-  //     });
-  //   });
-  // }
-
-  componentDidMount() {}
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        {this.state.current_user.id === "" ? (
-          <UserForm handleUserLogin={this.handleUserLogin} />
-        ) : (
-          <div>
-            <Header current_location={this.state.current_location} />
-            <MapBox
-              current_user={this.state.current_user}
-              tracks={this.state.tracks}
-              track_id={this.state.track_id}
-              track_num={this.state.track_num}
-              map={this.state.map}
-              handleState={this.handleState}
-            />
-            <Menu
-              currentUser={this.state.current_user}
-              map={this.state.map}
-              tracks={this.state.tracks}
-              trackNum={this.state.track_num}
-            />
-          </div>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div className={styles.root}>
+      {this.state.current_user.id === "" ? (
+        <UserForm handleUserLogin={this.handleUserLogin} />
+      ) : (
+        <div>
+          <Header current_location={currentLocation} />
+          <MapBox
+            current_user={currentUser}
+            tracks={tracks}
+            track_num={trackNum}
+            map={map}
+          />
+          <Menu
+            currentUser={currentUser}
+            map={map}
+            tracks={tracks}
+            trackNum={trackNum}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
-
-export default withStyles(styles, { withTheme: true })(App);
