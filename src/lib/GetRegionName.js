9bf63a005fbd8985eb61c6a0e83fb48e5d3e0d8d
k1105 <kanapon1105@yahoo.co.jp>
2021-08-06 00:48:24 +0900

    出力を大文字に変更

--- src/lib/GetRegionName.js
+++ src/lib/GetRegionName.js
@@ -13,7 +13,7 @@ export default async function GetRegionName(lng, lat) {
     access_token;
   try {
     const response = await axios.get(url);
-    return response.data.features[0].text.split(" ")[0];
+    return response.data.features[0].text.split(" ")[0].toUpperCase();
   } catch (err) {
     console.log(err);
     return "???";
