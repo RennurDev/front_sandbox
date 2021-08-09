d3d7a0468a39aba9cccb39b6fd486808be3f416c
k1105 <kanapon1105@yahoo.co.jp>
2021-08-09 23:38:07 +0900

    encoded_trackの初期化

--- src/lib/EncodeTrack.js
+++ src/lib/EncodeTrack.js
@@ -1,7 +1,6 @@
 export default function EncodeTrack(data) {
   if (data !== []) {
-    alert(data);
-    let encoded_track;
+    let encoded_track = "";
     for (let i = 0; i < data.length - 1; i++) {
       if (data[i]) {
         encoded_track += data[i] + ":";
@@ -9,6 +8,7 @@ export default function EncodeTrack(data) {
     }
     encoded_track += data[data.length - 1];
     // lng,lat:lng,lat...の形式に変換
+    alert(encoded_track);
     return encoded_track;
   } else {
     console.log("error happend while encoding track");
