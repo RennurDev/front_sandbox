6636f7b178d2612efd28ad00e31f4e9b4c86a1d8
k1105 <kanapon1105@yahoo.co.jp>
2021-08-10 10:04:55 +0900

    コメントの追加

--- src/lib/EncodeTrack.js
+++ src/lib/EncodeTrack.js
@@ -1,6 +1,6 @@
 export default function EncodeTrack(data) {
   if (data !== []) {
-    let encoded_track = "";
+    let encoded_track = ""; //NOTE: 空文字を代入しておかないとundefinedという文字列が生じる(hatake511522/front_sandbox/#96)
     for (let i = 0; i < data.length - 1; i++) {
       if (data[i]) {
         encoded_track += data[i] + ":";
