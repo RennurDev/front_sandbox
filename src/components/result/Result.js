c6e2319f95a5038a2fa12d1f107bab757b113965
Kanata Yamagishi <kanata@KanatanoMacBook-Air.local>
2021-08-15 13:02:12 +0900

    テスト環境用の設定

--- src/components/result/Result.js
+++ src/components/result/Result.js
@@ -2,8 +2,8 @@ import projectMercator from "../../lib/ProjectMercator";
 import { Grid } from "@material-ui/core";
 import "../../App.css";
 /*  testData: テスト時はこの場所にjsonファイルを追加すること */
-// import data from "./yamanotesen.json";
-// import decodeTrack from "../../lib/DecodeTrack";
+import data from "./yamanotesen.json";
+import decodeTrack from "../../lib/DecodeTrack";
 
 const styles = {
   root: {
@@ -65,8 +65,8 @@ export const Result = ({
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
-  const projectedTrack = projectMercator(posHistory, 50);
-  //const projectedTrack = projectMercator(decodeTrack(data.data), 50);
+  //const projectedTrack = projectMercator(posHistory, 50);
+  const projectedTrack = projectMercator(decodeTrack(data.data), 50);
 
   let dataString = "M " + projectedTrack[0][0] + " " + projectedTrack[0][1];
   for (let i = 1; i < projectedTrack.length; i++) {
@@ -87,30 +87,14 @@ export const Result = ({
         xmlSpace="preserve"
         style={styles.canvas}
       >
-        <g>
+        <g style={styles.container}>
           <path className="path" d={dataString} />
         </g>
       </svg>
-      <div style={styles.region}>
-        <Grid container spacing={2}>
-          <Grid xs={1} />
-          <Grid xs={5}>
-            <img
-              style={styles.img}
-              src={process.env.PUBLIC_URL + "/petampEye.svg"}
-              alt="petampButton"
-            />
-          </Grid>
-          <Grid xs={5}>
-            <h1>{currentRegion}</h1>
-          </Grid>
-          <Grid xs={1} />
-        </Grid>
-      </div>
       <div style={styles.text}>
         <Grid container spacing={2}>
-          <Grid xs={1} />
-          <Grid xs={8}>
+          <Grid item xs={1} />
+          <Grid item xs={8}>
             <h3>
               ALTITUDE : 0m
               <br />
@@ -122,14 +106,30 @@ export const Result = ({
           </Grid>
         </Grid>
       </div>
+      <div style={styles.region}>
+        <Grid container spacing={2}>
+          <Grid item xs={1} />
+          <Grid item xs={5}>
+            <img
+              style={styles.img}
+              src={process.env.PUBLIC_URL + "/petampEye.svg"}
+              alt="petampButton"
+            />
+          </Grid>
+          <Grid item xs={5}>
+            <h1>{currentRegion}</h1>
+          </Grid>
+          <Grid item xs={1} />
+        </Grid>
+      </div>
       <div style={styles.bottom}>
         <Grid container spacing={2}>
-          <Grid xs={1} />
-          <Grid xs={5}>
+          <Grid item xs={1} />
+          <Grid item xs={5}>
             <p>SHARE</p>
           </Grid>
-          <Grid xs={1} />
-          <Grid xs={5}>
+          <Grid item xs={1} />
+          <Grid item xs={5}>
             <p>
               <input
                 type="submit"
