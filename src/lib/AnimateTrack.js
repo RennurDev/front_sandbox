7297cd39c317a807f6fe3b396b94ab28f0342ed8
k1105 <kanapon1105@yahoo.co.jp>
2021-09-11 21:56:55 +0900

    コメントの追加

--- src/lib/AnimateTrack.js
+++ src/lib/AnimateTrack.js
@@ -1,8 +1,10 @@
 export default function AnimateTrack(map, id, data) {
-  const posOnTrack = (t) => {
-    try {
-      const i = parseInt(t) % data.length;
-      const delta = t - parseInt(t);
+  const posOnTrack = (time) => {
+    if (time) {
+      //NOTE: tの初期化完了前に呼び出される場合, tが渡されないことがある.
+      /* 大まかな処理: 動点はどの時間も, 点iから点i+1の線分上に位置する. そこで変数iでどの線分上に居るのかを定め, deltaでその線分の中でも点iからどの程度離れた位置に居るのかを定めている. */
+      const i = parseInt(time) % data.length; //タイムスタンプの少数点以下を切り捨て, trackの配列の長さで剰余を計算. 一定時間間隔でiが更新され, data.length周期でサイクルする.
+      const delta = time - parseInt(time); // 0 < delta < 1.
       const pos = [];
       if (i + 1 < data.length) {
         //i+1が存在する場合
@@ -18,13 +20,11 @@ export default function AnimateTrack(map, id, data) {
         type: "Point",
         coordinates: [pos[0][0], pos[0][1]],
       };
-    } catch (e) {
-      console.log(e);
     }
   };
 
   const animateMarker = (timestamp) => {
-    map.getSource(id).setData(posOnTrack(timestamp / 100));
+    map.getSource(id).setData(posOnTrack(timestamp / 300));
 
     // Request the next frame of the animation.
     requestAnimationFrame(animateMarker);
